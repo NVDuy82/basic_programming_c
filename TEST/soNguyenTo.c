9a0717bb9b87afc65821043855db28da5f0085c8
NVDuy82 <91014420+NVDuy82@users.noreply.github.com>
2022-02-11 09:44:09 +0700

    Update soNguyenTo.c

--- TEST/soNguyenTo.c
+++ TEST/soNguyenTo.c
@@ -3,21 +3,28 @@
 
 char check(int n){
   if(n < 2) return 0;
-  for(int i = 2; i <= round(sqrt(n)); i++){
+  if(n == 2) return 0;
+  for(int i = 3; i <= round(sqrt(n)); i += 2){
     if(!(n % i)) return 0;
   }
   return 1;
 }
 
 int main() {
-  unsigned int n;
-    printf("Nhap vao so nguyen duong n: ");
-    scanf("%d", &n);
+  unsigned int l;
+	scanf("%d", &l);
 
-  printf("%d", n);
-  if(check(n))
-    printf(" la so nguyen to!");
-  else
-    printf(" khong la so nguyen to!");
+  unsigned int n[l];
+  for(int i = 0; i < l; i++){
+	  scanf("%d", &n[i]);
+  }
+
+  for(int i = 0; i < l; i++){
+	  if(check(n[i]))
+      printf("YES");
+    else
+      printf("NO");
+    printf("\n");
+  }  
   return 0;
 }
