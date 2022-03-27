3664c62aac42151d029d064e465b4168d95d7b7d
NVDuy82 <nguyenvanduy74175@gmail.com>
2022-03-27 21:44:54 +0000

    update

--- TEST/GhepHinh.c
+++ TEST/GhepHinh.c
@@ -7,7 +7,14 @@ void swap(int *a, int *b){
   *b = temp;
 }
 
+void swapHcn(int *hcn1, int *hcn2){
+  for(int i = o; i <= 1; i++){
+    swap(hcn1 + i, hcn2 + i);
+  }
+}
+
 void sort(int *hcn){
+  // sort A -> Z
   if (hcn[0] > hcn[1]) swap(&hcn[0], &hcn[1]);
 }
 
@@ -28,17 +35,17 @@ int main() {
   int edge = sqrt(s);
   if (edge == sqrt(s)){    
     if (hcn[0][1] == hcn[1][1] && hcn[1][1] == hcn[2][1] && hcn[2][1] == edge){
+      // cac canh lon cua hinh vuong = edge
       printf("YES");
       return 0;
-    } else {      
+    } else {
       for(int i = 1; i < 3; i++){
         if (hcn[i][1] == edge){
-          for(int j = 0; j < 2; j++){
-            swap(&hcn[0][j], &hcn[i][j]);
-          }
+          swapHcn(hcn[0], hcn[i]); // di chuyen hcn co canh = edge len hcn[0]
         }
       }
-      if (hcn[0][1] == edge){        
+      if (hcn[0][1] == edge){
+        // 2 hcn con lai co canh = edge - hcn[0][0] 
         hcn[0][0] = edge - hcn[0][0];
         for(int i = 0; i < 2; i++){
           for(int j = 0; j < 2; j++){
