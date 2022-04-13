7deefbb309a595e8df4f0736977c07fa9cf82896
NVDuy82 <nguyenvanduy74175@gmail.com>
2022-04-13 14:15:05 +0000

    update comment

--- TEST/doiVe0.c
+++ TEST/doiVe0.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 void swap_0_1(int *a){
-  *a = 1 - *a;
+  *a = 1 - *a;  // swap 0 <-> 1
 }
 
 int main() {
   int n;
   do{
     scanf("%d", &n);
-  } while(n < 1 || 10 < n);
+  } while(n < 1 || 10 < n);  // enter n with  1 <= n <= 10
   int arr[n][n];
   for(int i = 0; i < n; ++i){
     for(int j = 0; j < n; ++j){
       do{
         scanf("%d", &arr[i][j]);
-      } while(arr[i][j] != 0 && arr[i][j] != 1);
+      } while(arr[i][j] != 0 && arr[i][j] != 1);  // enter arr[i][j] = 0 / 1
     }
   }
   printf("n = %d\n", n);
 
-  int step = 0;
+  int step = 0;   // output
 
+  // if number 0 < number 1  ->  swap all
   int count_0 = 0, count_1 = 0;
   for(int i = 0; i < n; ++i){
     for(int j = 0; j < n; ++j){
@@ -35,32 +36,21 @@ int main() {
     //  swap...
     for(int i = 0; i < n; ++i){
       for(int j = 0; j < n; ++j){
-        swap_0_1(&arr[i][j]);
+        swap_0_1(&arr[i][j]);  // 0 <-> 1
       }
     }
     step++;
-    printf("n = %d\n", n);
   }
-  printf("n = %d\n", n);
-  for(int i = 0; i < n; ++i){
-    for(int j = 0; j < n; ++j){
-      printf("%d ", arr[i][j]);
-    }
-    printf("\n");
-  }
-  printf("???\n");
 
+  // change 0 --> 1
   int start_row, start_col, end_row, end_col;
   for(int start_row = 0; start_row < n; ++start_row){
     for(int start_col = 0; start_col < n; ++start_col){
       if(arr[start_row][start_col] == 1){
-        printf("%d  %d  %d  %d\n", start_row, start_col, end_row, end_col);
         end_row = start_row;
-        printf("%d  %d  %d  %d\n", start_row, start_col, end_row, end_col);
         for(int j = start_col; j < n; ++j){
           if(arr[start_row][j] == 0){
             end_col = j - 1;
-            printf("%d  %d  %d  %d\n", start_row, start_col, end_row, end_col);
             end_row++;
             break;
           }
@@ -69,8 +59,6 @@ int main() {
           for(int j = start_col; j <= end_col; ++j){
             if(arr[i][j] == 0){
               end_row--;
-              printf("%d  %d  %d  %d\n", start_row, start_col, end_row, end_col);
-              printf("OK\n");
               step++;
               i = n;
               break;
@@ -79,7 +67,6 @@ int main() {
             }
           }
         }
-        printf("%d  %d  %d  %d\nend\n", start_row, start_col, end_row, end_col);
         //  swap...
         for(int i = start_row; i <= end_row; ++i){
           for(int j = start_col; j <= end_col; ++j){
@@ -88,13 +75,6 @@ int main() {
         }
       }
     }
-  }
-
-  for(int i = 0; i < n; ++i){
-    for(int j = 0; j < n; ++j){
-      printf("%d ", arr[i][j]);
-    }
-    printf("\n");
   }
 
   printf("%d", step);
