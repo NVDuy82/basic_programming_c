714a38421bb5d21158a772a75d37364c96d42bc0
Nguyen Van Duy <91014420+NVDuy82@users.noreply.github.com>
2023-02-20 01:49:04 +0700

    Update gcd_cua_day_so.c

--- TEST/gcd_cua_day_so.c
+++ TEST/gcd_cua_day_so.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 
-void swapNum(int *a, int *b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+void swapNum(int * a, int * b) {
+  int temp = * a;
+  * a = * b;
+  * b = temp;
 }
 
-int lcm(int a, int b){
-  if (!(a > b)) swapNum(&a, &b);
+int lcm(int a, int b) {
+  if (!(a > b)) swapNum( & a, & b);
   //  a > b
 
-  for(int i = 1; i < b; ++i){
-    if (!(b % i) && !((a*i) % b)) return a*i;
+  for (int i = 1; i < b; ++i) {
+    if (!(b % i) && !((a * i) % b)) return a * i;
   }
-  
+
   return a * b;
 }
 
-void gcdArr(){
+void gcdArr() {
   int n;
-  do{
-    scanf("%d", &n);
-  } while (n < 2 || 1000 < n);  // 2 <= n <= 1 000
+  do {
+    scanf("%d", & n);
+  } while (n < 2 || 1000 < n); // 2 <= n <= 1 000
 
   int a[n];
-  int b[n+1];
-  for(int i = 0; i < n; ++i){
-    do{
-      scanf("%d", &a[i]);
-    } while (a[i] < 1 || 10000 < a[i]);  // 1 <= a[i] <= 10 000
+  int b[n + 1];
+  for (int i = 0; i < n; ++i) {
+    do {
+      scanf("%d", & a[i]);
+    } while (a[i] < 1 || 10000 < a[i]); // 1 <= a[i] <= 10 000
   }
 
   b[0] = a[0];
-  for(int i = 1; i < n; ++i){
-    b[i] = lcm(a[i-1], a[i]);
+  for (int i = 1; i < n; ++i) {
+    b[i] = lcm(a[i - 1], a[i]);
   }
-  b[n] = a[n-1];
+  b[n] = a[n - 1];
 
-  for(int i = 0; i < n+1; ++i){
+  for (int i = 0; i < n + 1; ++i) {
     printf("%d ", b[i]);
   }
   puts("");
@@ -45,9 +45,9 @@ void gcdArr(){
 
 int main() {
   int cases; // number of cases
-  scanf("%d", &cases);
+  scanf("%d", & cases);
 
-  for(int i = 0; i < cases; ++i){
+  for (int i = 0; i < cases; ++i) {
     gcdArr();
   }
 
